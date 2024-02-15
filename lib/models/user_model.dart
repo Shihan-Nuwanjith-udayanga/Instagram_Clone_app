b938e92f0dcb48ea250bc56c40ad44958aaccc96
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2024-02-15 16:40:32 +0530

    create user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,46 @@
+class UserModel {
+  //schema of the users collection
+  final String uid;
+  final String email;
+  final String userName;
+  final String bio;
+  final String profilePic;
+  final List followers;
+  final List following;
+
+  //constructor
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.userName,
+    required this.bio,
+    required this.profilePic,
+    required this.followers,
+    required this.following,
+  });
+
+  //this method will convert the userData in to a json object
+  Map<String, dynamic> toJSON() {
+    return {
+      'uid': uid,
+      'email': email,
+      'userName': userName,
+      'bio': bio,
+      'profilePic': profilePic,
+      'followers': followers,
+      'following': following,
+    };
+  }
+
+  //this method will convert the json object to user data
+  factory UserModel.fromJSON(Map<String, dynamic> json) {
+    return UserModel(
+        uid: json['uid'],
+        email: json['email'],
+        userName: json['userName'],
+        bio: json['bio'],
+        profilePic: json['profilePic'],
+        followers: json['followers'],
+        following: json['following']);
+  }
+}
