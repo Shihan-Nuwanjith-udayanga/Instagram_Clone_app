85f734ed39282f52ef9c83f435a1082f06bd9211
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2024-02-15 14:01:39 +0530

    create login page

---
+++ lib/screens/authentication/register_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/button.dart';
+import 'package:instagram_clone/widgets/text_field.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Image.asset(
+                  'assets/logo.png',
+                  height: 70,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Stack(
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundColor: secondaryColor,
+                      backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadiusDirectional.circular(30),
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add_a_photo,
+                            color: mainOrange,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                TextInputFiled(
+                  controller: _emailController,
+                  hintText: "Enter Email",
+                  isPassword: false,
+                  inputKeyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextInputFiled(
+                  controller: _passwordController,
+                  hintText: "Enter a Password",
+                  isPassword: true,
+                  inputKeyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextInputFiled(
+                  controller: _userNameController,
+                  hintText: "Enter a Username",
+                  isPassword: false,
+                  inputKeyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextInputFiled(
+                  controller: _bioController,
+                  hintText: "Enter Bio",
+                  isPassword: false,
+                  inputKeyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SubmitButton(
+                  text: "Register",
+                  onPressed: () {},
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Log in",
+                        style: TextStyle(
+                            color: mainOrange, fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
