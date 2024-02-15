7a98a7188d19f148235e0d998b8e0f8bd2c89a8c
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2024-02-15 14:02:33 +0530

    create a app colors const values

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
+const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const mainOrange = Color.fromRGBO(251, 173, 80, 1);
+const mainYellow = Color.fromRGBO(252, 204, 99, 1);
+const mainMaroon = Color.fromRGBO(205, 72, 107, 1);
+const mainBlue = Color.fromRGBO(76, 104, 215, 1);
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
