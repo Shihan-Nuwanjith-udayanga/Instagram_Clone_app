f3e330aa4304909e4205000d087c49a801459cae
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2024-02-15 16:40:49 +0530

    create responsive layout

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/constants.dart';
+
+class ResponsiveLayout extends StatefulWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return widget.webScreenLayout;
+        } else {
+          return widget.mobileScreenLayout;
+        }
+      },
+    );
+  }
+}
